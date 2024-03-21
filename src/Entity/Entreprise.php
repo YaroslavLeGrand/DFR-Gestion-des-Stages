5ff697fd54560b1a9ade8a44ecaddbefb423ae66
YaroslavLeGrand <yaroslav.follet@campus-la-chataigneraie.org>
2024-03-21 17:48:27 +0100

    permier migate

---
+++ src/Entity/Entreprise.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EntrepriseRepository::class)
+ */
+class Entreprise
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $RS;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Ville;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Pays;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Activitee;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRS(): ?string
+    {
+        return $this->RS;
+    }
+
+    public function setRS(string $RS): self
+    {
+        $this->RS = $RS;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->Ville;
+    }
+
+    public function setVille(string $Ville): self
+    {
+        $this->Ville = $Ville;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->Pays;
+    }
+
+    public function setPays(string $Pays): self
+    {
+        $this->Pays = $Pays;
+
+        return $this;
+    }
+
+    public function getActivitee(): ?string
+    {
+        return $this->Activitee;
+    }
+
+    public function setActivitee(string $Activitee): self
+    {
+        $this->Activitee = $Activitee;
+
+        return $this;
+    }
+}
