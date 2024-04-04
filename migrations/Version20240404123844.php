5eec745f45ea1efdbbd28177dc8dc78043ca4fc2
YaroslavLeGrand <132467006+YaroslavLeGrand@users.noreply.github.com>
2024-04-04 15:06:01 +0200

    Crud email ok

---
+++ migrations/Version20240404123844.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240404123844 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE email CHANGE email_libelle email_libelle VARCHAR(250) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE email CHANGE email_libelle email_libelle VARCHAR(255) NOT NULL');
+    }
+}
--- src/Form/TelephoneType.php
+++ src/Form/TelephoneType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Telephone;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +12,7 @@ class TelephoneType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Telephone',TextType::class)
+            ->add('Telephone')
         ;
     }
 
