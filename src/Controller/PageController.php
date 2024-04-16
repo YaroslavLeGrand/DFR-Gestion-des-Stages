95b10c6202abf7e91e2ce57ec41b4e683910c772
NriderCF <delattrelouis76@gmail.com>
2024-04-16 09:20:14 +0200

    commentage session start

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Controller;
-session_start();
+//session_start();
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
